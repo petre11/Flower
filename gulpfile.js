355c9c2d2a8ce2bdfc42d3dbc52aecc13bc82335
Piotr Korowicki <Piotrkorowicki@gmail.com>
2017-04-20 19:54:49 +0200

    Finall version

--- gulpfile.js
+++ gulpfile.js
@@ -1,17 +1,27 @@
 var gulp = require("gulp");
 var sass = require("gulp-sass");
 var sourcemaps = require("gulp-sourcemaps");
+var browserSync = require('browser-sync').create();
 
-gulp.task("sass", function() {
-  return gulp.src("scss/*.scss")
-    .pipe(sourcemaps.init())
-    .pipe(sass({errLogToConsole: true}))
-    .pipe(sourcemaps.write())
-    .pipe(gulp.dest("css"));
+gulp.task('sass', function() {
+  return gulp.src('app/scss/**/*.scss') // Gets all files ending with .scss in app/scss
+    .pipe(sass())
+    .pipe(gulp.dest('app/css'))
+    .pipe(browserSync.reload({
+      stream: true
+    }));
 });
 
-gulp.task("watch", function () {
-  gulp.watch("scss/*.scss", ["sass"]);
+gulp.task("watch",['browserSync','sass'], function () {
+  gulp.watch("app/scss/**/*.scss", ["sass"]);
 });
 
 gulp.task("default", ["sass", "watch"]);
+
+gulp.task('browserSync', function() {
+  browserSync.init({
+    server: {
+      baseDir: 'app'
+    },
+  });
+});
